7f17388653b48cfa895921b9902a3aa494d51a69
ElesinaTatyana <elesina.tan@yandex.ru>
2021-03-20 16:43:27 +0400

    add news

--- src/main/java/com/eltavi/recipegrampath/repository/RecipeRepository.java
+++ src/main/java/com/eltavi/recipegrampath/repository/RecipeRepository.java
@@ -1,7 +1,11 @@
 package com.eltavi.recipegrampath.repository;
 
 import com.eltavi.recipegrampath.entity.Recipe;
+import com.eltavi.recipegrampath.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findAllByUser(User user);
 }
--- src/main/java/com/eltavi/recipegrampath/service/RecipeService.java
+++ src/main/java/com/eltavi/recipegrampath/service/RecipeService.java
@@ -2,6 +2,7 @@ package com.eltavi.recipegrampath.service;
 
 import com.eltavi.recipegrampath.dto.RecipeDto;
 import com.eltavi.recipegrampath.entity.Recipe;
+import com.eltavi.recipegrampath.entity.User;
 
 import java.util.List;
 
@@ -13,4 +14,5 @@ public interface RecipeService {
     boolean deleteRecipe(Long id, Long userId);
     RecipeDto like(Long id);
     RecipeDto dislike(Long id);
+    List<RecipeDto> getNews(User user, int size);
 }
